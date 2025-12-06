9754f1ad627f1f658be056aa6fc7311c148d7dcc
Ursula Mayer <ursulamayer123@gmail.com>
2025-12-06 15:11:21 -0300

    Ejercicio clase 10

---
+++ clase10/10ejercicio6.js
@@ -0,0 +1,8 @@
+const prompt = require ('prompt-sync')();
+let primerNumero = 75
+let segundoNumero = 7
+if (primerNumero > 10 && segundoNumero > 10){
+    console.log(" Los números son mayor a 10.");
+} else {
+    console.log("Los números son menor a 10.");
+}
