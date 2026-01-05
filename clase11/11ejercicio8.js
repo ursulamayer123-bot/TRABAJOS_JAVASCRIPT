45d8dab30115a9d32faa5b51aeea84f6a8177a84
Ursula Mayer <ursulamayer123@gmail.com>
2026-01-05 16:47:41 -0300

    Ejercicio 8 clase 11

--- clase11/11ejercicio8.js
+++ clase11/11ejercicio8.js
@@ -1,6 +1,8 @@
 const prompt = require ('prompt-sync')();
-let numero = parseInt (prompt("Ingrese un numero entre el 1 al 100."));
-for (let i = 1; i <=100; i++);
+let numero = parseInt (prompt("Ingrese un numero entre el 1 al 100: "));
+ console.log("Los múltiplos de " + numero + " son: ");
+for (let i = 1; i <=100; i++){
 if (i % numero === 0){
 console.log(i);
+}
 }
