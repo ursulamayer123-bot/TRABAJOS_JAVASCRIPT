33bcff3d8ff8eb4ccc2470f3ca7cd8541ea64396
Ursula Mayer <ursulamayer123@gmail.com>
2026-01-05 17:36:38 -0300

    Ejercicio 10 clase 11

---
+++ clase11/11ejercicio10.js
@@ -0,0 +1,22 @@
+const prompt = require ('prompt-sync')();
+const numeroSecreto = Math.floor(Math.random() * 10) + 1;
+console.log("Pienso en un numero del 1 al 10. Tenes 3 intentos para adivinar.");
+let ganaste = false; 
+
+for (let i = 1; i <= 3; i++) {
+    let intento = parseInt(prompt("Intento " + i + ": ¿Que número es?: "));
+    if (intento === numeroSecreto) {
+        console.log("¡FELICITACIONES! ¡Adivinaste el número!");   
+       ganaste = true; 
+       
+        break; 
+
+    } else {
+        
+        console.log("Incorrecto");
+    }
+}
+
+if (ganaste === false) { 
+    console.log("El número secreto era: " + numeroSecreto);
+}
