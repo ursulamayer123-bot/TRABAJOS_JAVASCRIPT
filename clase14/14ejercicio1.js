e492752fe1d4150e7e1442f94cfe372587f32d47
Ursula Mayer <ursulamayer123@gmail.com>
2026-01-16 19:36:18 -0300

    Ejercicios clase14

--- clase14/14ejercicio1.js
+++ clase14/14ejercicio1.js
@@ -2,4 +2,4 @@ const prompt = require ('prompt-sync')();
 const precioConIVA = (precio, porcentaje) => precio + (precio + porcentaje /100);
 let Productoprecio = parseFloat (prompt("Ingresa el precio del producto: "));
 let porcentajeIVA = parseFloat (prompt("Ingresa el procentaje de IVA: "));
-console.log("El precio es: " + precioconIVA (Productoprecio, porcentajeIVA));
+console.log("El precio es: " + precioConIVA (Productoprecio, porcentajeIVA));
